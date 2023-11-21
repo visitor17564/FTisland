6320d12546327b88e88a6bb3068ade4447c7268c
wolfram0407 <77099657+wolfram0407@users.noreply.github.com>
2023-11-21 18:05:40 +0900

    [ADD] : express 기본값 추가

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+// import
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+// router import
+
+const { errorHandler } = require("./middlewares/error.handler.js");
+const app = express();
+const port = 3000;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(flash());
+
+// router middleware
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.use((err, req, res, next) => {
+  errorHandler(err.message, req, res);
+});
+
+app.listen(port, () => {
+  console.log(port, "listening on port " + port);
+});
