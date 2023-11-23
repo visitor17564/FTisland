2bae5275026287535abd6821541625c4b38f4a50
visitor17564 <lanlanloo17564@gmail.com>
2023-11-23 21:08:45 +0900

    [EDIT] 잘될거야 항상그랬듯이

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const cookieParser = require("cookie-parser");
 const postsRouter = require("./routes/posts.js");
 const authRouter = require("./routes/auth.js");
 const mypageRouter = require("./routes/mypage.js");
+const mainRouter = require("./routes/main.router.js");
 
 //const { errorHandler } = require("./middlewares/error.handler.js");
+const path = require("path");
 const app = express();
 const port = 3000;
 
@@ -20,13 +22,8 @@ const publicDirectoryPath = path.join(__dirname, "public");
 app.use(express.static(publicDirectoryPath));
 const temp = ["서울", "경기", "인천", "강원"];
 // router middleware
-app.use("/api", [authRouter, mypageRouter]);
-
-app.get("/", async (req, res) => {
-  const a = await Users.findAll();
-  console.log(a);
-  res.send("Welcome");
-});
+app.use("/api", [authRouter, mypageRouter, postsRouter]);
+app.use("/", [mainRouter]);
 
 // app.use((err, req, res, next) => {
 //   errorHandler(err.message, req, res);
