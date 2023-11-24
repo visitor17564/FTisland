cbd35cba98f4e464b1740daa3b5a08c90ed91440
kimyungyeom <kgk1599@gmail.com>
2023-11-24 17:55:09 +0900

    [ADD] : 회원가입, 로그인 validation 추가

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { Users } = require("../models");
 const { redis } = require("../config/config");
+const { body } = require("express-validator");
+const { validatorErrorCheck } = require("../middlewares/validatorErrorCheck-middleware");
 require("dotenv").config();
 
 // auth.js - global variables
@@ -13,28 +15,25 @@ const refreshTokenSecretKey = process.env.REFRESH_TOKEN_SECRET_KEY;
 
 // routers
 // 회원가입 API
-router.post("/auth/signup", async (req, res) => {
-  // 이메일, 유저네임, 비밀번호, 확인용비밀번호를 데이터로 넘겨받음
-  const { email, username, password, confirmPassword } = req.body;
+router.post("/auth/signup", [
+  // 빈 입력란 여부 체크 및 앞뒤 공백 제거
+  body("email").notEmpty().trim().withMessage("이메일이 비어있습니다."),
+  body("username").notEmpty().trim().withMessage("이름이 비어있습니다."),
+  body("password").notEmpty().trim().withMessage("비밀번호가 비어있습니다."),
+  body("confirmPassword").notEmpty().trim().withMessage("확인용 비밀번호가 비어있습니다."),
 
-  // 빈 입력란 여부 체크
-  if (!email || !username || !password || !confirmPassword) {
-    return res.status(401).send({
-      success: false,
-      errorMessage: "입력란 중 비어있는 곳이 있습니다."
-    });
-  }
+  // 이메일 형식 체크
+  body("email").isEmail().isLength({max:30}).withMessage("올바른 이메일 형식이 아닙니다."),
+  
+  // 비밀번호 6자 이상, 일치 여부 확인
+  body("password").isLength({min:6}).withMessage("비밀번호는 최소 6자 이상입니다."),
+  body("confirmPassword").custom((confirmPassword, {req}) => {
+    if(confirmPassword !== req.body.password) {
+      return false;
+    }
+    return true;
+  }).withMessage("비밀번호가 서로 일치하지 않습니다."),
 
-  // 비밀번호 최소 6자 이상, 비밀번호 일치 여부 확인
-  if (password.length < 6 || password !== confirmPassword) {
-    return res.status(401).send({
-      success: false,
-      errorMessage: "비밀번호가 최소 6자 이상이어야 하며, 서로 일치해야 합니다."
-    });
-  }
-
-  // 이메일 정규식
-  const emailRegex = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
   // 이메일 형식 체크
   body("email").isEmail().isLength({max:30}).withMessage("올바른 이메일 형식이 아닙니다."),
   
@@ -73,8 +72,17 @@ router.post("/auth/signup", async (req, res) => {
   res.redirect("/login");
 });
 
+
 // 로그인 API
-router.post("/auth/login", async (req, res) => {
+router.post("/auth/login", [
+  // 빈 입력란 여부 체크 및 앞뒤 공백 제거
+  body("email").notEmpty().trim().withMessage("이메일이 비어있습니다."),
+  body("password").notEmpty().trim().withMessage("비밀번호가 비어있습니다."),
+
+  // 이메일 형식 체크
+  body("email").isEmail().isLength({max:30}).withMessage("올바른 이메일 형식이 아닙니다.")
+
+], validatorErrorCheck, async (req, res) => {
   // 이메일, 비밀번호를 데이터로 넘겨받음
   const { email, password } = req.body;
 
@@ -90,16 +98,16 @@ router.post("/auth/login", async (req, res) => {
   }
 
   // 비밀번호 서로 일치여부 확인
-  const matchPassword = await bcrypt.compare(password, user.password);
+  const hash = user.password
+  const isValidPass = await bcrypt.compare(password, hash);
 
-  if (!matchPassword) {
+  if (!isValidPass) {
     return res.status(401).send({
       success: false,
       errorMessage: "비밀번호가 일치하지 않습니다."
     });
   }
   const accessToken = jwt.sign({ userId: user.userId }, accessTokenSecretKey, { expiresIn: "1h" });
-
   const refreshToken = jwt.sign({ userId: user.userId }, refreshTokenSecretKey, { expiresIn: "1d" });
   redis.connect();
 
